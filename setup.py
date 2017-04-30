214b88f77bf2d96a42b385367837b5d7d0526ab2
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2017-04-30 21:58:19 +0000

    add Python version check

--- setup.py
+++ setup.py
@@ -8,7 +8,28 @@
 # Written by Lawrence D'Oliveiro <ldo@geek-central.gen.nz>.
 #-
 
+import sys
 import distutils.core
+from distutils.command.build import \
+    build as std_build
+
+class my_build(std_build) :
+    "customization of build to perform additional validation."
+
+    def run(self) :
+        try :
+            class dummy :
+                pass
+            #end dummy
+            dummy.__doc__ = "something"
+        except AttributeError :
+            sys.stderr.write("This module requires Python 3.3 or later.\n")
+            sys.exit(-1)
+        #end try
+        super().run()
+    #end run
+
+#end my_build
 
 distutils.core.setup \
   (
@@ -19,4 +40,8 @@ distutils.core.setup \
     author_email = "ldo@geek-central.gen.nz",
     url = "http://github.com/ldo/qahirah",
     py_modules = ["qahirah"],
+    cmdclass =
+        {
+            "build" : my_build,
+        },
   )
